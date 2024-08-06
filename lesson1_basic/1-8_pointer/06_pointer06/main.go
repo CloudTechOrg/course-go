d7b710c170b032e313cc11c398bfd1444083627b
kymx1983 <kymx1983@gmail.com>
2024-08-07 01:07:20 +0900

    add lesson01

---
+++ lesson1_basic/1-8_pointer/06_pointer06/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	Name string
+	Age  int
+}
+
+func (person *Person) setValue(name string, age int) {
+	person.Name = name
+	person.Age = age
+}
+
+func main() {
+	person := Person{
+		Name: "テスト太郎",
+		Age:  30,
+	}
+	person.setValue("テスト二郎", 25)
+	fmt.Println(person)
+
+	fmt.Println("---初期の値を表示---")
+	fmt.Println(person)
+
+	person.setValue("テスト二郎", 25)
+
+	fmt.Println("---メソッド実行後の値を表示---")
+	fmt.Println(person)
+}
